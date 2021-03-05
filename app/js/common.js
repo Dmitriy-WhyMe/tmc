3ab38566c6ba7583a2564bd04493b5428f9515cd
Dmitriy <dima.ivahnoff@mail.ru>
2021-03-05 16:57:32 +0300

    coments

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,11 @@
 (function ($) {
 	$(document).ready(function () {
-
-
+		$('.multiple-items').slick({
+			infinite: true,
+			slidesToShow: 6,
+			slidesToScroll: 1,
+			prevArrow: $('.arrow_next'),
+			nextArrow: $('.arrow_prev')
+		});
 	});
 })(jQuery);
