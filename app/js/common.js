0f7224a1c09714742745e1e736813fd99fcceccd
Dmitriy <dima.ivahnoff@mail.ru>
2021-03-09 11:46:04 +0300

    desc-ipad_pro

--- app/js/common.js
+++ app/js/common.js
@@ -5,7 +5,39 @@
 			slidesToShow: 6,
 			slidesToScroll: 1,
 			prevArrow: $('.arrow_next'),
-			nextArrow: $('.arrow_prev')
+			nextArrow: $('.arrow_prev'),
+			responsive: [
+				{
+					breakpoint: 1441,
+					settings:{
+						slidesToShow: 5,
+						slidesToScroll: 1,
+				  	}
+				},
+				{
+					breakpoint: 1025,
+					settings:{
+						slidesToShow: 4,
+						slidesToScroll: 1,
+				  	}
+				},
+				{
+					breakpoint: 835,
+					settings:{
+						slidesToShow: 3,
+						slidesToScroll: 1,
+				  	}
+				}
+			]
+		});
+		$('input[type="tel"]').inputmask({
+			mask: "+7 ([9][9][9]) [9][9][9]-[9][9]-[9][9]",
+			greedy: false
+		});
+		jQuery(document).ready(function($) {
+			$('#pop').magnificPopup({
+				type: 'inline'
+			});
 		});
 	});
 })(jQuery);
