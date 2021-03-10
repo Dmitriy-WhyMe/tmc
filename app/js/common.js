25bd24b81ab6caf098f624c5b5e4d753981b9a85
Dmitriy <dima.ivahnoff@mail.ru>
2021-03-10 16:43:26 +0300

    dop_pravki

--- app/js/common.js
+++ app/js/common.js
@@ -37,22 +37,108 @@
 				}
 			]
 		});
+		//Mask
 		$('input[type="tel"]').inputmask({
 			mask: "+7 ([9][9][9]) [9][9][9]-[9][9]-[9][9]",
 			greedy: false
 		});
+		//Popup
 		jQuery(document).ready(function($) {
 			$('#pop').magnificPopup({
 				type: 'inline'
 			});
 		});
-		window.onload = init;
-		function init(){
-			var button = document.getElementById("coco")
-			button.onclick = handleButtonClick;
+		jQuery(document).ready(function($) {
+			$('#pop_1').magnificPopup({
+				type: 'inline'
+			});
+		});
+		jQuery(document).ready(function($) {
+			$('#pop_2').magnificPopup({
+				type: 'inline'
+			});
+		});
+		//Cookie
+		function getCookie() {
+			var cookies = document.cookie.match ( '(^|;) ?adultconfirmed=([^;]*)(;|$)' );
+			if (cookies) { $('.coco').hide(); }
+			}
+			$('#coco').click(function(event) {
+			event.preventDefault();
+			document.cookie = 'adultconfirmed=yes; path=/; max-age=604800';
+			$('.coco').hide();
+			});
+		getCookie();
+		// menu
+		$('#main-menu li a').each(function () {      
+			var location = window.location.href 
+			var link = this.href                
+			var result = location.match(link);  
+			if(result != null) {                
+				$(this).addClass('active');    
+			}
+		});
+		// select
+		$(document).ready(function() {
+			$('select').niceSelect();
+		});
+		// maps
+
+		var map;
+		var arrMarkers = [];
+		var arrInfoWindows = [];
+		
+		function mapInit() {
+			var centerCoord = new google.maps.LatLng(55.748053042845335, 37.550249169656325);
+			var mapOptions = {
+				zoom: 7,
+				center: centerCoord,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+		
+			map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+			$.getJSON("../js/markers.json", {}, function (data) {
+				$.each(data.places, function (i, item) {
+		
+					var marker = new google.maps.Marker({
+						position: new google.maps.LatLng(item.lat, item.lng),
+						map: map,
+						title: item.title
+					});
+		
+					arrMarkers[i] = marker;
+					var infowindow = new google.maps.InfoWindow({
+						content: "<h3>" + item.title + "</h3><p>"
+					});
+		
+					arrInfoWindows[i] = infowindow;
+					google.maps.event.addListener(marker, 'click', function () {
+						infowindow.open(map, marker);
+					});
+		
+				});
+			});
 		}
-		function handleButtonClick() {
-			document.getElementsByClassName('coco')[0].style.display = 'none';
+		
+		function newLocation(newLat, newLng, newZoom) {
+			map.setCenter({
+				lat: newLat,
+				lng: newLng
+			});
+			map.setZoom(newZoom)
 		}
+		
+		$(document).on('click', '.maps__link', function (e) {
+			e.preventDefault();
+			newLocation($(this).data('lat'), $(this).data('long'), $(this).data('zoom'));
+		});
+		
+		
+		$(function () {
+			if($('#map-canvas').length) {
+				mapInit();
+			}
+		});
 	});
 })(jQuery);
